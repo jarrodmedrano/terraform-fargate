e3cf02e999f2b31d015581328934202768ca230a
Jarrod Medrano <jmedran@gmail.com>
2023-09-27 16:17:37 -0500

    remove passport

--- api/server.js
+++ api/server.js
@@ -1,39 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
-const passport = require("passport");
-const BearerStrategy = require("passport-azure-ad").BearerStrategy;
-const config = require("./config");
 
 const PORT = process.env.PORT || 5000;
-
-// API scope required to access /api endpoint (app registration)
-const SCOPES = ["access_as_user"];
-
-const options = {
-  identityMetadata: `https://${config.metadata.authority}/${config.credentials.tenantID}/${config.metadata.version}/${config.metadata.discovery}`,
-  clientID: config.credentials.clientID,
-  issuer: `https://${config.metadata.authority}/${config.credentials.tenantID}/${config.metadata.version}`,
-  validateIssuer: config.settings.validateIssuer,
-  audience: config.credentials.audience,
-  passReqToCallback: config.settings.passReqToCallback,
-  loggingLevel: config.settings.loggingLevel,
-  loggingNOPII: config.settings.loggingNOPII,
-  scope: SCOPES,
-};
-
-const bearerStrategy = new BearerStrategy(options, function (token, done) {
-  console.log("verifying token:", token);
-  return done(null, {}, token);
-});
-
-passport.use(bearerStrategy);
 
 const app = express();
 
 // log all requests
 app.use(morgan("common"));
-
-app.use(passport.initialize());
 
 // enable CORS
 app.use((req, res, next) => {
@@ -57,19 +30,10 @@ app.get("/check", (req, res) => {
   res.status(200).send("Hello, this is the check endpoint!");
 });
 
-app.get(
-  "/api",
-  passport.authenticate("oauth-bearer", { session: false }),
-  (req, res) => {
-    // service relies on the name claim
-    res.status(200).json({
-      name: req.authInfo["name"],
-      issuer: req.authInfo["iss"],
-      audience: req.authInfo["aud"],
-      scope: req.authInfo["scp"],
-    });
-  }
-);
+app.get("/api", (req, res) => {
+  // service relies on the name claim
+  res.status(200).send("api");
+});
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
